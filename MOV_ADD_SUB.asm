b8f82ece90de65b496fef376a634b110206c7cf0
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-15 13:56:44 +0300

    Update MOV_ADD_SUB.asm

--- MOV_ADD_SUB.asm
+++ MOV_ADD_SUB.asm
@@ -17,3 +17,20 @@ MOV AX, DS            ; REG, SREG
 
 MOV DS, AX            ; SREG, REG
 
+
+
+
+
+
+MOV [1000h], 1   ; memory, immediate address [1000h] consist of 1;
+ ADD AX, [1000h]  ; REG, memory  AX=AX+1
+ ADD AX, [1000h]
+ ADD AX, [1000h]   
+ 
+ ADD AX, AX       ; REG, REG  
+    
+ ADD [1000h], 3h  ; memory immediate
+ 
+ ADD AX, 4h       ; REG,immediate
+ 
+ ret
