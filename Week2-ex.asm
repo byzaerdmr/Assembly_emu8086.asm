d10a46f8d030590200434bb94224bb77eb369d5f
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-21 15:18:00 +0300

    Update Week2-ex.asm

--- Week2-ex.asm
+++ Week2-ex.asm
@@ -7,3 +7,29 @@ MOV [2000h], 34h
 
 RET
 
+
+;ex2
+
+MOV CL, 11h
+MOV DL, 22h
+
+MOV AL, CL
+MOV CL, DL
+MOV DL, AL
+
+RET
+
+
+;ex3
+                 
+MOV AX, 100h
+MOV DS, AX
+                 
+MOV [500h], 98h
+MOV [501h], 52h 
+
+ADD DL, [500h] 
+ADD DL, [501h] 
+
+MOV [502h], DL
+RET
