121da8154f90216ba314863afe4318ab80865c83
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-22 15:35:50 +0300

    Update ADC_SBB.asm

--- ADC_SBB.asm
+++ ADC_SBB.asm
@@ -16,5 +16,29 @@ ADC BX, [1006h]  ;BX +1006 + CF
 
 MOV [1008h], AX
 MOV [100Ah], BX   
-
 ret                
+
+
+
+
+; 3b B2D0 5E00
+; 1b 3B9A CA00
+
+
+MOV word ptr [1000h], 5E00h
+MOV word ptr [1002h], 0B2D0h
+
+;1b 3B9A CA00
+MOV word ptr [1004h], 0CA00h
+MOV word ptr [1006h], 3B9Ah
+
+MOV AX, [1000h]
+SUB AX, [1004h]   
+MOV BX, [1002h]
+SBB BX, [1006h]        
+
+MOV [1008h], AX
+MOV [100Ah], BX   
+ret                
+
+
