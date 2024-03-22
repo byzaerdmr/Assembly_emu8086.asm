0fb37336bb15efcabb8ce027b4f551b271142e55
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-22 09:18:22 +0300

    Update Week3-ex.asm

--- Week3-ex.asm
+++ Week3-ex.asm
@@ -1,1 +1,62 @@
+;Sum of the number between 1 to 100
+MOV BX, 100h
+MOV DS, BX
+
+MOV CX, 100d  ; or 64h  
+MOV DX, 1     ;100
+
+label:
+       
+       
+ ADD AX, DX   ; AX, CX for memory efficiency!
+ INC DX      ;DEC DX
+
+LOOP label  
+
+ret
+
+
+
+
+;Sum of the even number between 1 to 100
+MOV BX, 100h
+MOV DS, BX
+
+MOV CX, 100d  ; or 64h    
+
+label:
+       
+       
+ ADD AX, CX 
+ DEC CX
+      
+
+
+LOOP label  
+
+
+ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
