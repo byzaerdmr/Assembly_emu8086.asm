4465aa741cc6853fcc21534bcc33ad1955c98a74
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-22 11:50:18 +0300

    Update Week4-ex.asm

--- Week4-ex.asm
+++ Week4-ex.asm
@@ -1,1 +1,15 @@
+MOV [1000h], 0C2h      
+
+CMP [1000h], 0C2h
+JZ true
+JNZ false
+
+true:
+    MOV [1100h], 0FFh  
+JMP toend 
+false:
+     MOV [1100h], 0AAh   
+                      
+ toend:
+ ret
 
