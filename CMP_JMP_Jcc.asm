3cbf4b06f3f140981f8fed8752d516aca8509609
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-03-22 10:10:37 +0300

    Update CMP_JMP_Jcc.asm

--- CMP_JMP_Jcc.asm
+++ CMP_JMP_Jcc.asm
@@ -1,1 +1,29 @@
+ include 'emu8086.inc'
+   ORG 100h   
+   
+   MOV AL, 5
+   JMP label1    ; jump over 2 lines! 
+   
+   
+   PRINT 'Not Jumped!'
+   MOV AL, 0
+   label1:
+   PRINT 'Got Here!'
+   RET
+
+
+
+; jmp using for endless loop
+
+MOV AL, 5
+MOV CL, 7 
+
+label:
+
+INC AL
+DEC CL
+
+JMP label
+
+ret
 
