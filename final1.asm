8e18cff5d26af7837721248548c3872edadb7b41
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-05-31 10:13:55 +0300

    Update final1.asm

--- final1.asm
+++ final1.asm
@@ -1,3 +1,10 @@
+Write a program that compares two 4 byte sized unsigned integers written on 0100:1000h and 0100:1004h 
+and if integer that written on 0100:1000h is bigger than integer written on 0100:1004h,
+then subtract two numbers, if not, then add two numbers. 
+Then copy the result to 0100:1008h. First integer is 4,000,000,000 and second integer is 2,000,000,000.
+
+
+
 ;4b EE6B 2800
 ;2b 7735 9400   
 
