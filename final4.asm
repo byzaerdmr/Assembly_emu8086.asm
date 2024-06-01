226c81fe0fb7224a6cb3a0b27b7686f3337bf3cf
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-06-01 14:45:59 +0300

    Update final4.asm

--- final4.asm
+++ final4.asm
@@ -37,3 +37,36 @@ JNE label
 
 RET
 myname DB 'Beyza Erdemir',0
+
+
+
+
+Write a program that writes your name to the screen backwards. You must use a DB variable.  (e.g. name DB "John DOE" )  
+
+LEA BX, myname
+
+MOV AL, [BX]
+
+MOV AH, 0Eh
+MOV CX, 0
+
+count:
+    INC CX
+    MOV AL, [BX]
+    INC BX
+    CMP AL, 0 
+JNE count
+DEC BX
+    
+label: 
+
+    MOV AL, [BX]
+    INT 10h 
+    DEC BX  
+    
+LOOP label
+
+
+RET
+
+myname DB 'Beyza Erdemir',0
