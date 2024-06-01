d5aadeaf610745cc47074661628888eb88eecfaf
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-06-01 22:17:07 +0300

    Update final6.asm

--- final6.asm
+++ final6.asm
@@ -15,3 +15,7 @@ label:
     LOOP label
 
 ret
+
+
+Write a program that rotate the word stored at 0100:0300h memory address to left until it will be negative and save the rotation count in CX.
+If the last value is bigger than signed value A100h then write AAh to 0100:1000h, if not write BBh to 0100:1000h.
