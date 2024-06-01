93d2796be5cd173e8614a76ad72e3bc9219e74b8
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-06-01 21:50:30 +0300

    Update final6.asm

--- final6.asm
+++ final6.asm
@@ -1,1 +1,17 @@
+Write a program that reads a word from 0100:0300h memory address and count the 1s (ones) in this value. Then write count to DX.
 
+MOV word ptr[300h], 0F0F0h
+
+MOV AX, [300h]
+
+MOV CX, 16
+
+label:  
+
+    ROL AX, 1  ;rotate left and rotate carry
+    JNC jump   ;CF 0 oldugunda 
+    INC DX 
+    jump:
+    LOOP label
+
+ret
