7b26025dc916019774031af5e8e18194af95bf7a
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-06-01 17:43:19 +0300

    Update final5.asm

--- final5.asm
+++ final5.asm
@@ -12,5 +12,14 @@ SHL BL, 1
 ADD AL, BL
 
 MOV [1002h], AL
+RET
+
+
+
+Write a program that divide signed value of 0100:3000h memory address by 4d using shift instructions, and saves the result to the memory address 0100:3000h.
+
+MOV [3000h], 20
+
+SAR [3000h], 2  ;diveded by 4
 
 RET
